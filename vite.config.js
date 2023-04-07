23a21d2f534ad17a75973dd28b69f33bedd5b13b
Diana Martinez <diananerdoficial@gmail.com>
2023-04-07 02:33:32 -0600

    Remove old firmware and vscode debug flag

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig(({ command }) => {
 
   const isServe = command === 'serve'
   const isBuild = command === 'build'
-  const sourcemap = isServe || !!process.env.VSCODE_DEBUG
+  const sourcemap = isServe
 
   return {
     plugins: [
@@ -22,16 +22,12 @@ export default defineConfig(({ command }) => {
           // Main-Process entry file of the Electron App.
           entry: 'electron/main/index.js',
           onstart(options) {
-            if (process.env.VSCODE_DEBUG) {
-              console.log(/* For `.vscode/.debug.script.mjs` */'[startup] Electron App')
-            } else {
-              options.startup()
-            }
+            options.startup()
           },
           vite: {
             build: {
               sourcemap,
-              minify: isBuild,
+              minify: true, // isBuild,
               outDir: 'dist-electron/main',
               rollupOptions: {
                 external: Object.keys('dependencies' in pkg ? pkg.dependencies : {}),
@@ -49,7 +45,7 @@ export default defineConfig(({ command }) => {
           vite: {
             build: {
               sourcemap: sourcemap ? 'inline' : undefined, // #332
-              minify: isBuild,
+              minify: true, // isBuild,
               outDir: 'dist-electron/preload',
               rollupOptions: {
                 external: Object.keys('dependencies' in pkg ? pkg.dependencies : {}),
@@ -61,7 +57,7 @@ export default defineConfig(({ command }) => {
       // Use Node.js API in the Renderer-process
       renderer(),
     ],
-    server: process.env.VSCODE_DEBUG && (() => {
+    server: (() => {
       const url = new URL(pkg.debug.env.VITE_DEV_SERVER_URL)
       return {
         host: url.hostname,
