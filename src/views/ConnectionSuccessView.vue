0a7815c0d7beed216f44a0a71b614548e12e0ce8
Diana Martinez <diananerdoficial@gmail.com>
2023-04-28 04:10:17 -0600

    Add connect to network step and base styles

---
+++ src/views/ConnectionSuccessView.vue
@@ -0,0 +1,8 @@
+<template>
+    Connection Success
+    <RouterLink to="/connect-account">Connect Account</RouterLink>
+</template>
+
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
--- src/views/SetupConnectionView.vue
+++ src/views/SetupConnectionView.vue
@@ -3,13 +3,26 @@
     <div v-if="!connected">
         Device not connected
     </div>
+    <form @submit.prevent="connect" v-else-if="!connecting">
+        <select v-if="networks.length" v-model="network">
+            <option v-for="n in networks" :key="n" :value="n">
+                {{ n }}
+            </option>
+        </select>
+        <div v-else>
+            Loading networks...
+        </div>
+        <input v-if="network" :type="showPass ? 'text' : 'password'" v-model="password" />
+        <input v-if="network" v-model="showPass" type="checkbox" />
+        <button v-if="network">Connect</button>
+    </form>
     <div v-else>
-        <pre>{{ serial }}</pre>
+        Connecting...
     </div>
 </template>
 
 <script setup>
-import { onMounted, onBeforeUnmount, ref, watch } from 'vue'
+import { onMounted, ref, computed } from 'vue'
 import { useRouter } from 'vue-router'
 import { useEsptool } from '@/libs/esptool'
 const router = useRouter()
@@ -21,21 +34,55 @@ const {
   writeSerial
 } = useEsptool()
 
-const next = () => {
-    router.push('/connect-account')
+const prevSerial = ref('')
+const lastSerial = ref('')
+const showPass = ref(null)
+const connecting = ref(false)
+
+const network = ref('')
+const password = ref('')
+
+const networks = computed(() => {
+    const reg = /\d+\)\s.*[^\[0m]$/gm
+    const str = lastSerial.value
+    const list = [...str.matchAll(reg)].flat().map((e) => e.replace('\r', '')).filter((e) => !e.includes('\[0m')).map((e) => e.split(' ')[1])
+    return list
+})
+
+const connection = computed(() => {
+    const status = lastSerial.value.includes('WiFi Connected')
+    return status
+})
+
+const fetchNetworks = () => {
+    lastSerial.value = serial.value.replace(prevSerial.value, '')
+    prevSerial.value = serial.value
+    writeSerial('networks\n')
+    if (!network.value) {
+        setTimeout(fetchNetworks, 2500)
+    }
 }
 
-const t = ref(null)
+const waitConnection = () => {
+    lastSerial.value = serial.value.replace(prevSerial.value, '')
+    prevSerial.value = serial.value
+    if (!connection.value) {
+        setTimeout(waitConnection, 2500)
+    } else {
+        connecting.value = false
+        router.push('/connection-success')
+    }
+}
+
+const connect = () => {
+    connecting.value = true
+    writeSerial(`join ${network.value} ${password.value}\n`)
+    waitConnection()
+}
 
 onMounted(() => {
     readSerial()
     reset()
-    t.value = setInterval(() => {
-        writeSerial('help\n')
-    }, 3000)
-})
-
-onBeforeUnmount(() => {
-    clearInterval(t.value)
+    fetchNetworks()
 })
 </script>
