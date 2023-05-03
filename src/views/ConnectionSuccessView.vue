4629db2ce33e131209ef1ecff4788576c943ed80
Diana Martinez <diananerdoficial@gmail.com>
2023-05-03 04:08:20 -0600

    Add wifi icon to successful connection

--- src/views/ConnectionSuccessView.vue
+++ src/views/ConnectionSuccessView.vue
@@ -2,7 +2,7 @@
     <h2>Dispositivo conectado</h2>
     <p>Ahora puedes vincular el dispositivo a tu cuenta.</p>
     <div class="flex">
-        <img class="icon" src="@/assets/success.svg" alt="success" />
+        <img class="icon" src="@/assets/wifi.svg" alt="wifi" />
         <RouterLink class="btn" to="/connect-account">Continuar</RouterLink>
     </div>
 </template>
