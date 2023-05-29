798d616ea561cb372756a48f454d7e8d36808444
Diana Martinez <diananerdoficial@gmail.com>
2023-05-28 20:44:33 -0600

    Fix final link

--- src/views/ConnectAccountView.vue
+++ src/views/ConnectAccountView.vue
@@ -3,6 +3,6 @@
     <p>Accede a tu cuenta de Platzi en el navegador, luego haz click en vincular.</p>
     <div class="flex">
         <img class="icon rotate" src="@/assets/sync.svg" alt="sync" />
-        <a class="btn" target="_blank" href="https://space.platzi.com/stations/add?code=LS83F3w99YAu0ICQBt5xH0rGCtYsWd9u">Vincular</a>
+        <a class="btn" target="_blank" href="https://space.platzi.com/stations/add/LS83F3w99YAu0ICQBt5xH0rGCtYsWd9u">Vincular</a>
     </div>
 </template>
